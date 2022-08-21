844e606a44cdf3de5855210f6aed94206ce2d336
Светлана М <karenina2909@mail.ru>
2022-08-21 14:47:26 +0300

    задача2

---
+++ задача2сравнение первого числа с корнем второго/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача2 программа которая проверяет является ли первое число, квадратом второго.
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+    Console.WriteLine($"{num1} является квадратом {num2}");
+else
+{
+    Console.WriteLine($"{num1} не является квадратом {num2}");
+}    
