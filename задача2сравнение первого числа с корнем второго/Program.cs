f1b20aef26136de20843ac2e1b471abeb06fff43
Светлана М <karenina2909@mail.ru>
2022-08-21 15:24:05 +0300

    зад4

--- задача2сравнение первого числа с корнем второго/Program.cs
+++ задача2сравнение первого числа с корнем второго/Program.cs
@@ -1,4 +1,5 @@
-﻿//Задача2 программа которая проверяет является ли первое число, квадратом второго.
+﻿/*
+//Задача2 программа которая проверяет является ли первое число, квадратом второго.
 Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
@@ -9,4 +10,23 @@ if (num1 == num2 * num2)
 else
 {
     Console.WriteLine($"{num1} не является квадратом {num2}");
-}    
+}
+   
+
+// Задача3 На вход одается число N,на выходе нужно указать все целые число от -N до N.
+Console.WriteLine("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+//задача 4 вводится трехзначное число, нужно вывести последнюю цифру.
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 10;
+Console.WriteLine($" последняя цифра {num} это {result}");
